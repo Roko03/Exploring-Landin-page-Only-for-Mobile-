996cf89e3850cb60bbe8b60206542854d71a02fc
Roko03 <rokoponjarac2@gmail.com>
2021-01-17 18:15:21 +0100

    first commit

---
+++ java.js
@@ -0,0 +1,11 @@
+function dropDown(src){
+    window.location=src;
+}
+
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 2,
+    spaceBetween:160,
+    slidesOffsetBefore:80,
+    slidesOffsetAfter:85,
+    freeMode:true,
+});
